4df5b1c452eb2c90c45b7e8f25d3e65730c8c794
David Tran <dhtran@hotmail.com>
2018-08-18 13:26:25 -0500

    Done

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,5 @@
 var db = require('../models');
 
-<<<<<<< HEAD
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
@@ -13,17 +12,17 @@ module.exports = function (app) {
     res.render('login')
   });
 
-  const authCheck = (req, res, next) => {
-    if (!req.db.user) {
-      res.redirect('/auth/login')
-    } else {
-      next();
-    }
-  }
+  // const authCheck = (req, res, next) => {
+  //   if (!req.db.user) {
+  //     res.redirect('/auth/login')
+  //   } else {
+  //     next();
+  //   }
+  // }
 
-  app.get('/schedule', authCheck, (req, res) => {
-    res.render('schedule');
-  })
+  // app.get('/schedule', authCheck, (req, res) => {
+  //   res.render('schedule');
+  // })
 
   // app.get("/", function(req, res) {
   //   db.Example.findAll({}).then(function(dbExamples) {
@@ -47,32 +46,4 @@ module.exports = function (app) {
   // app.get("*", function(req, res) {
   //   res.render("404");
   // });
-=======
-module.exports = function(app) {
-	// Load index page
-	app.get('/', function(req, res) {
-		// db.Example.findAll({}).then(function(dbExamples) {
-		// 	res.render('index', {
-		// 		msg: 'Welcome!',
-		// 		examples: dbExamples
-		// 	});
-		// });
-		res.render('index');
-	});
-
-	// Load example page and pass in an example by id
-	app.get('/example/:id', function(req, res) {
-		// db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-		// 	res.render('example', {
-		// 		example: dbExample
-		// 	});
-		// });
-		res.send('hello');
-	});
-
-	// Render 404 page for any unmatched routes
-	app.get('*', function(req, res) {
-		res.render('404');
-	});
->>>>>>> e9029bccf48a3508ee517a318702748f041d9b81
 };
