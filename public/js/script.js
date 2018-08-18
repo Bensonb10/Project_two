4de58324d022c95500bf0469e5703faddc104c95
Push <crashjohnson@gmail.com>
2018-08-18 14:05:58 -0500

    created functions to handle automation of slider creation based on db data

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,3 @@
-
 // write your script from here
 // $("#range_16").ionRangeSlider({
 //     grid: true,
@@ -11,19 +10,56 @@
 // });
 
 
-moment.locale("en-GB");
+sliderModify("Push", "11:00", "16:00", "2018-08-18", "#range_16");
+sliderStatic("Push", "11:00", "16:00", "2018-08-18", "#range_17");
 
-var $range = $("#range_16");
-var start = moment("2016-10-02 08:00", "YYYY-MM-DD HH:mm");
-var end = moment("2016-10-02 20:00", "YYYY-MM-DD HH:mm");
 
-$range.ionRangeSlider({
-    type: "double",
-    grid: true,
-    min: start.format("x"),
-    max: end.format("x"),
-    step: 1800000,                // 30 minutes in ms
-    prettify: function (num) {
-        return moment(num, 'x').format("h:mm A");
-    }
-});
+function sliderModify(empId, timeIn, timeOut, shiftDate, elementId) {
+    moment.locale("en-GB");
+
+    var $range = $(elementId);
+    var start = moment(`${shiftDate} 08:00`, "YYYY-MM-DD HH:mm");
+    var end = moment(`${shiftDate} 22:00`, "YYYY-MM-DD HH:mm");
+    let startFrom = moment(`${shiftDate} ${timeIn}`, "YYYY-MM-DD HH:mm");
+    let startTo = moment(`${shiftDate} ${timeOut}`, "YYYY-MM-DD HH:mm");
+
+
+    $range.ionRangeSlider({
+        type: "double",
+        grid: true,
+        min: start.format("x"),
+        max: end.format("x"),
+        from: startFrom.format("x"),
+        to: startTo.format("x"),
+        step: 1800000, // 30 minutes in ms
+        prettify: function (num) {
+            return moment(num, 'x').format("h:mm A");
+        }
+    });
+};
+
+function sliderStatic(empId, timeIn, timeOut, shiftDate, elementId) {
+    moment.locale("en-GB");
+
+    var $range = $(elementId);
+    var start = moment(`${shiftDate} 08:00`, "YYYY-MM-DD HH:mm");
+    var end = moment(`${shiftDate} 22:00`, "YYYY-MM-DD HH:mm");
+    let startFrom = moment(`${shiftDate} ${timeIn}`, "YYYY-MM-DD HH:mm");
+    let startTo = moment(`${shiftDate} ${timeOut}`, "YYYY-MM-DD HH:mm");
+
+
+    $range.ionRangeSlider({
+        type: "double",
+        grid: true,
+        min: start.format("x"),
+        max: end.format("x"),
+        from: startFrom.format("x"),
+        to: startTo.format("x"),
+        from_fixed: true,
+        to_fixed: true,
+        step: 1800000, // 30 minutes in ms
+        prettify: function (num) {
+            return moment(num, 'x').format("h:mm A");
+        }
+    });
+};
