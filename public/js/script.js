753a5ed127bae5a304ebc8494e8b17743d2cac72
Bensonb10 <bensonb10@gmail.com>
2018-08-18 14:05:20 -0500

    pull commit

--- public/js/script.js
+++ public/js/script.js
@@ -10,20 +10,27 @@
 //     max_postfix: "+"
 // });
 
+function sliderTool(empId, timeIn, timeOut, shiftDate) {
+    moment.locale("en-GB");
+    var startTime = timeIn;
+    var endTime = timeOut;
 
-moment.locale("en-GB");
+    var $range = $("#range_16");
+    var start = moment("2016-10-02 08:00", "YYYY-MM-DD HH:mm");
+    var end = moment("2016-10-02 20:00", "YYYY-MM-DD HH:mm");
+    var startFrom = moment("2016-10-02 " + startTime, "YYYY-MM-DD HH:mm");
+    var endFrom = moment("2016-10-02 " + endTime, "YYYY-MM-DD HH:mm");
 
-var $range = $("#range_16");
-var start = moment("2016-10-02 08:00", "YYYY-MM-DD HH:mm");
-var end = moment("2016-10-02 20:00", "YYYY-MM-DD HH:mm");
-
-$range.ionRangeSlider({
-    type: "double",
-    grid: true,
-    min: start.format("x"),
-    max: end.format("x"),
-    step: 1800000,                // 30 minutes in ms
-    prettify: function (num) {
-        return moment(num, 'x').format("h:mm A");
-    }
-});
+    $range.ionRangeSlider({
+        type: "double",
+        grid: true,
+        min: start.format("x"),
+        max: end.format("x"),
+        from: startFrom.format("x"),
+        to: endFrom.format("x"),
+        step: 1800000,                // 30 minutes in ms
+        prettify: function (num) {
+            return moment(num, 'x').format("h:mm A");
+        }
+    });
+}
