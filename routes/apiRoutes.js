d29dd81023bd659f83d17e486837199e008f03a9
Mohammed Chawla <chawla.mohammed@gmail.com>
2018-08-20 17:10:35 -0500

    have post and delete api routes working need to test update routes and need to write get routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,30 @@
 var db = require('../models');
 
 module.exports = function(app) {
-	// Get request to get all employees
-	// app.get('/api/examples', function(req, res) {
-	// 	db.Example.findAll({}).then(function(dbExamples) {
-	// 		res.json(dbExamples);
-	// 	});
-	// });
+	// Get request to find all employees & their contact info in database (view this whole table)
+	app.get('/api/employees', function(req, res) {
+		db.EmployeeTable.findAll({}).then(function(dbEmployeeTable) {
+			res.json(dbEmployeeTable);
+		});
+	});
+
+	//Get request to find all available and unavailable times posted by employees (view whole avail table)
+	app.get('/api/employees', function(req, res) {
+		db.AvailTable.findAll({}).then(function(dbAvailTable) {
+			res.json(dbAvailTable);
+		});
+	});
+
+	//Get request to get schedule (all shifts for the day/week)
+	app.get('/api/employees', function(req, res) {
+		db.AvailTable.findAll({}).then(function(dbAvailTable) {
+			res.json(dbAvailTable);
+		});
+	});
+
+
+
+
 
 	
 	//post request to post user data to database
@@ -14,7 +32,8 @@ module.exports = function(app) {
 
 	//posting user data to Employee table
 		db.EmployeeTable.create({
-			name: req.body.name,
+			firstName: req.body.firstName,
+			lastName : req.body.lastName,
 			isAdmin: req.body.isAdmin,
 			email: req.body.email,
 			phone: req.body.phone,
@@ -54,29 +73,64 @@ module.exports = function(app) {
 		});
 	});
 			
-					
+
+	
+
+
+
 
 	// Delete an employee by id
 	app.delete('/api/employees/:id', function(req, res) {
-		db.EmployeeTable.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-			res.json(dbExample);
+		db.EmployeeTable.destroy({ where: { id: req.params.id } }).then(function(dbEmployeeTable) {
+			res.json(dbEmployeeTable);
 		});
 	});
 
 
-	// Delete an avail by id
+	// Delete an availability time by id
 	app.delete('/api/avail/:id', function(req, res) {
-		db.AvailTable.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-			res.json(dbExample);
+		db.AvailTable.destroy({ where: { id: req.params.id } }).then(function(dbAvailTable) {
+			res.json(dbAvailTable);
 		});
 	});
 
-	// Delete an schedule by id
+	// Delete a schedule by id
 	app.delete('/api/schedule/:id', function(req, res) {
-		db.ScheduleTable.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-			res.json(dbExample);
+		db.ScheduleTable.destroy({ where: { id: req.params.id } }).then(function(dbScheduleTable) {
+			res.json(dbScheduleTable);
 		});
 	});
+
+
+
+
+
+	
+	//update an employees info by id
+	app.put('/api/employees/:id', function(req, res) {
+		db.EmployeeTable.update({firstName: req.body.firstName},{
+			where: {id: req.params.id}}).then(function(dbEmployeeTable) {
+			res.json(dbEmployeeTable);
+		});
+	});
+	//update availability by id
+	app.put('/api/avail', function(req, res) {
+		db.AvailTable.update(req.body,{
+			where: {id: req.body.id}}).then(function(dbAvailTable) {
+			res.json(dbAvailTable);
+		});
+	});
+	//update schedule for day/week by id
+	app.put('/api/schedule', function(req, res) {
+		db.ScheduleTable.update(req.body,{
+			where: {id: req.body.id}}).then(function(dbScheduleTable) {
+			res.json(dbScheduleTable);
+		});
+	});
+
+
+
+
 
 
 };
