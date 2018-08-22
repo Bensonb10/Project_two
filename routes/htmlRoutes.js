445bec02787c2cf3f7c49777b4764481fe496299
David Tran <dhtran@hotmail.com>
2018-08-22 18:33:56 -0500

    Drop down menu

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,12 +2,12 @@ var db = require('../models');
 
 module.exports = function (app) {
 	// Checks incoming request to see if user is logged in. If not it will redirect to login handlebars with a message
-	function checkAuthentication(req,res,next){
-		if(req.isAuthenticated()){
+	function checkAuthentication(req, res, next) {
+		if (req.isAuthenticated()) {
 			//req.isAuthenticated() will return true if user is logged in
 			next();
-		} else{
-			res.render("login", {error: "You have to be sign in."});
+		} else {
+			res.render("login", { error: "You have to be sign in." });
 		}
 	}
 
@@ -46,13 +46,76 @@ module.exports = function (app) {
 
 	// GET: /create
 	// Load Create page if user is autheticated
-	app.get('/create', checkAuthentication, (req, res) => {
-		db.EmployeeTable.findAll({include: db.AvailTable}).then((dbresult) => {
-			let hbsObj = {employees: dbresult}
-			// console.log(dbresult)
-			// console.log("--------------------------")
-			console.log(hbsObj.employees[0].AvailTables[0].avail)
-			res.render('create', hbsObj)
+	app.get('/create', (req, res) => {
+		db.ScheduleTable.findAll({ include: db.EmployeeTable }).then((dbScheduleTable) => {
+			console.log(dbScheduleTable[0])
+
+			if (dbScheduleTable.length !== 0) {
+				// Send hbsobj to make a sliders for each schedule/shift   
+
+				// Send hbsobj to "Select Employee" dropdown btn that contains list<employees> available for that shift
+				let monday = dbScheduleTable.filter((x) => x.date === '2018-08-20');
+				let tuesday = dbScheduleTable.filter((x) => x.date === '2018-08-21');
+				let hbsObj = {
+					monday: monday,
+					tuesday: tuesday
+				};
+				res.render('create', hbsObj);
+			} else {
+				res.render('create');
+			}
+
 		})
+
+		// db.EmployeeTable.findAll({include: db.AvailTable}).then((dbResult) => {
+		// 	console.log(dbResult[0].AvailTables);
+
+			
+		// 	// for(AvailTables)
+		// 	// if(date === (shift date) && isAvail)
+		// 	// if(start >= (shift start time) && end <= (shift end time))
+		// 	// show employee
+
+			
+
+		// 	// console.log(hbsObj.employees[0].AvailTables[0].avail)
+
+			
+		// })
+
+		// 	const Op = db.Sequelize.Op;
+		// 	// Find all employees available for that shift
+		// 	db.AvailTable.findAll({
+
+		// 	// , {where: {
+		// 	// 	date: {
+		// 	// 		[Op.between]: [20180819, 20180820]
+		// 	// 	}
+		// 	// 	}
+		// 	// }
+		// }).then((dbAvailTable) => {
+		// 		if (dbAvailTable.length !== 0) {
+		// 			// Populate the "Select Employee"  
+		// 			console.log(dbAvailTable)
+		// 			let hbsObj = {shifts: dbAvailTable};
+
+		// 		} else {
+		// 			res.render('create');
+		// 		}
+		// 	});
 	});
+
+	app.post('/create', checkAuthentication, (req, res) => {
+		let currentDate = "2018-08-22";
+		let startShift = "0900";
+		let endShift = "2100"
+		db.ScheduleTable.create({
+			date: currentDate,
+			start: startShift,
+			end: endShift
+		}).then((dbScheduleTable) => {
+			req.flash('success_msg', 'Created a new shift');
+			res.redirect('/create');
+		})
+	})
 };
