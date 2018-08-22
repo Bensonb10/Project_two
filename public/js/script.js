a2764a5603889198f46d0a556bce9c6e96a3ab72
Push <crashjohnson@gmail.com>
2018-08-22 01:50:44 -0500

    create page mostly functioning. It creates unique ids for each element it creates withing a given day. The slider is not working (i think because of dom being loaded already)

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,6 @@
 
+let shiftsArr = [];
 
-// write your script from here
-// $("#range_16").ionRangeSlider({
-//     grid: true,
-//     type: 'double',
-//     min: 18,
-//     max: 70,
-//     from: 30,
-//     prefix: "Age ",
-//     max_postfix: "+"
-// });
 sliderModify('Push', '11:00', '16:00', '2018-08-18', '#range_16');
 
 sliderStatic('Push', '09:00', '16:00', '2018-08-18', '#range_17');
@@ -68,21 +59,27 @@ function modifyAccordion(date) {
 	//take the date value from the date picker and turn it into a moment object
 	let weekDay = moment(date);
 
+	//loop through each accordion header 
 	$('.sched-ul li.day-header').each(function(){
 		let wdReadable = weekDay.format('dddd: MMM Do');
 		let unixDay = weekDay.format('X');
 
+		//change the header text to the formatted date
 		$(this).find('.header-text').text(wdReadable);
-		$(this).data('id', unixDay);
+		//add a unique data-attribute using the unix formatted date
+		$(this).attr('data-id', unixDay);
+		$(this).find('.add-btn').attr('data-id', unixDay);
+		$(this).find('.collapsible-body').attr('data-id', 'cb-' + unixDay);
 
 		console.log(wdReadable)
+		//add 1 day to the date before moving on to the next element
 		weekDay.add(1,'d');
 	});
 
+	//This code adds the neccessary classes to open the first accordion element
 	$('.sched-ul li.day-header').first().addClass('active');
 	$('.sched-ul li.day-header.active .collapsible-header').addClass('active');
 	$('.sched-ul li.day-header.active .collapsible-body').attr('style','display: block;');
-
 }
 
 $('.datepicker').datepicker({
@@ -91,4 +88,77 @@ $('.datepicker').datepicker({
 
 		modifyAccordion(date);
 	}
+});
+
+function addModSlider(date, elementId) {
+	moment.locale('en-GB');
+
+	var $range = $('range_'+elementId);
+	var start = moment(`${date} 08:00`, 'YYYY-MM-DD HH:mm');
+	var end = moment(`${date} 22:00`, 'YYYY-MM-DD HH:mm');
+	let startFrom = moment(`${date} 12:00`, 'YYYY-MM-DD HH:mm');
+	let startTo = moment(`${date} 13:00`, 'YYYY-MM-DD HH:mm');
+
+
+	$range.ionRangeSlider({
+		type: 'double',
+		grid: true,
+		min: start.format('x'),
+		max: end.format('x'),
+		from: startFrom.format('x'),
+		to: startTo.format('x'),
+		step: 1800000, // 30 minutes in ms
+		prettify: function (num) {
+			return moment(num, 'x').format('h:mm A');
+		}
+	});
+
+}
+
+//add button genertates and inserts the slider/employee block
+$('.collapsible-header .add-btn').on('click', function(event){
+	event.stopPropagation();
+	let uniqueId = moment().format('x');
+	let scheduleDate = $(this).data('id');
+	let elementId = scheduleDate + '-' + uniqueId;
+
+	console.log(`${scheduleDate} - ${elementId}`)
+
+	addModSlider(scheduleDate,elementId);
+
+	$(`[data-id=cb-${scheduleDate}`).append(`
+	<div class="row shift-item-row">
+        <div class="col s12">
+            <ul id="create-page-schedule" class="collection sched-creation-collection" style="overflow: visible">
+                <li class="collection-item" data-id="1">
+                    <div class="row valign-wrapper">
+                        <div class="col s3">
+                            <!-- Dropdown button -->
+                            <a class="dropdown-button waves-effect waves-light btn blue" href="#" data-activates="dropdown-block">Select Employee<i class="material-icons white-text right">arrow_drop_down</i></a><ul id="dropdown-block" class="dropdown-content" style="width: 170.672px; position: absolute; top: 741.812px; left: 45px; display: none; opacity: 1;">
+                                <li>
+                                    <div class="row valign-wrapper">
+                                        <div class="col s4">
+                                            <i class="material-icons medium">face</i>
+                                        </div>
+                                        <div class="col s5">
+                                            Ben B.
+                                        </div>
+                                        <div class="col s3">
+                                            [4hrs]
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                            <!-- Dropdown button structure -->
+                        </div>
+                        <div class="col s9">
+                            <input id="range_16" />
+                        </div>
+                    </div>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+	`);
 });
